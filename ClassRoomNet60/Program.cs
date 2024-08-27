7f36812dae42cf4b5791b7d7a7861463b17b8923
RebinIsmail <149385501+RebinIsmail@users.noreply.github.com>
2024-08-27 15:58:57 +0200

    ne

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassRoomNet60;
+
+class Program
+{
+    static void Main()
+    {
+        ClassRoom classRoom = new ClassRoom()
+        {
+            ClassName = "3B",
+            SemesterStart = new DateTime(2019, 8, 26)
+        };
+        
+        classRoom.StudentList.Add(new Student("Rebin", 6, 15)); 
+        classRoom.StudentList.Add(new Student("Mads", 7, 10));
+
+        
+        Console.WriteLine($"Class Name: {classRoom.ClassName}");
+        Console.WriteLine($"Semester Start: {classRoom.SemesterStart}");
+        Console.WriteLine("Student List:");
+        foreach (Student Student in classRoom.StudentList)
+        {
+            Console.WriteLine($"- Name: {Student.Name}, Birthdate: {Student.Birthday}/{Student.BirthMonth}");
+        }
+    }
+    
+ 
+
+    
+}
